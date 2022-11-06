275a3a8ad02d24f2452b2a8a6954e15c48c5bf23
Michael Yiu <maplepie32@gmail.com>
2022-11-05 18:29:29 -0700

    created columns for models and created relationships between the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,38 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+  as: 'product_tag'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+
+  as: 'tag_of_product'
+})
+
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,20 +1,44 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500),json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+ try {
+  const categoryData = await Category.findByPk(req.params.id, {
+    include: [{ model: Product, through: ProductTag, as: 'category_products'}]
+  });
+
+  if(!categoryData) {
+    res.status(404).json({ message: 'No category found with this id!'});
+    return;
+  }
+
+  res.status(200).json(categoryData);
+ } catch(err) {
+  res.status(500).json(err);
+ }
+ 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Cateogry.create(req.body);
+    res.status
+  }
 });
 
 router.put('/:id', (req, res) => {
